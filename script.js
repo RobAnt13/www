109263d5bc94244b9ffd4e330cd8350bde76865e
RobAnt13 <robert.antony050607@gmail.com>
2022-11-13 16:27:43 +0100

    funkcia if else if

---
+++ script.js
@@ -0,0 +1,16 @@
+let position = "Administratr"
+
+if(position === "Programator") {
+    console.log("Aky je rozdiel medzi html, css a javascriptom.")
+} else if(position === "Administrator") {
+    console.log("Ake poznate druhy sieti")
+} else if(position === "Sekretarka") {
+    console.log("Ake pozname funkcie v exceli")
+} else {
+    console.log("Chyba, zavolajte programatora")
+}
+
+
+
+
+
