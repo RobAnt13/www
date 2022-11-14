461fc95054457e061489558f637087cf15697458
RobAnt13 <robert.antony050607@gmail.com>
2022-11-14 15:48:31 +0100

    Základy Javascriptu 2 Funkcie pokracovanie

--- script.js
+++ script.js
@@ -2,88 +2,72 @@
 
 // Funkcie
 
-/* function pozdrav1(){
-    console.log("Pozdrav 1")
- }
+// Undefined a null 
 
-pozdrav1()
+// Undefined v premenne
+/* let age
 
- let pozdrav2 = function(){
-    console.log("Pozdrav 2")
- }
-
- pozdrav2()
-
- let pozdrav3 = () => console.log("Pozdrav 3")
-
- pozdrav3() 
-
- // Funkcie - parameter a argument + šípková notácia
-
- let naDruhu = function(num){
-    console.log(num * num)
- }
-
- let naDruhu = (num) => console.log(num * num)
-
- naDruhu(2)
- naDruhu(5)
-
- let pozdravFunkcia = function(name){
-    console.log("Ahoj, ja som " + name)
- }
-
- let pozdravFunkcia = (name) => console.log("Ahoj, ja som " + name)
-
- pozdravFunkcia("David")
- pozdravFunkcia("Harry")
- pozdravFunkcia("Hermiona") */
-
-
- // Return vo funkcii
-
- let naDruhu = function(num){
-    let result = num * num
-    return result
- }
-
-let value1 = naDruhu(5)
-value1 = value1 + 50
-console.log(value1)
-
-let zvysovaciaFunkcia = function(num){
-    let result = num + 100
-    return result
+if(age === undefined){
+    console.log("Vyplnte vek")
+} else {
+    console.log(age)
 }
 
-let value2 = zvysovaciaFunkcia(50)
+// Undefined ako argument funkcie
+let mojaFunkcia = function(num){
+    console.log(num)
+}
+
+mojaFunkcia() 
+
+// Undefined v return
+let mojaFunkcia2 = function(num){
+    return num
+}
+
+let value = mojaFunkcia2()
+console.log(value)
+
+// Null ako priradena hodnota
+let myAge = 30
+myAge = null
+console.log(myAge) */
+
+// Funkcia s viacerými parametrami
+let sucet = function(a, b, c){
+    return a + b + c
+}
+
+let value = sucet(20, 30, 5)
+console.log(value)
+
+// Default hodnoty
+let game = function(name = "Anonymny", score = 0){
+    return "Meno: " + name + ", Score: " + score
+}
+
+let value2 = game("David", 50)
 console.log(value2)
 
-// Adult checker
+// Vstupný kód
 
-let adultChecker = function(age){
-    if(age >= 18){
-        let result = "Dospelý"
-        return result
+// Globálny scope (num1, num2, num3)
+// Lokálny scope (a, b, c)
+
+let num1 = 5
+let num2 = 8
+let num3 = 2
+
+let pristup = function(a, b, c){
+    if(a === num1 && b === num2 && c === num3){
+        console.log("Môžte vstúpiť")
     } else {
-        let result = "Dieťa"
-        return result
+        console.log("Vstup zakázaný")
     }
 }
 
-let value = adultChecker(12)
-console.log(value)
-
-if(value === "Dospelý"){
-    console.log("Výsledok kontroly veku: " + value)
-    console.log("Môžeš vstúpiť")
-} else {
-    console.log("Výsledok kontroly veku: " + value)
-    console.log("Nemôžeš vstúpiť")
-}
-
-
-
+pristup(5, 3, 9)
+pristup(5, 8, 2)
 
 
 
