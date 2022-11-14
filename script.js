b240e58b92b80a85acf3355ad5401bb236b6a9b8
RobAnt13 <robert.antony050607@gmail.com>
2022-11-14 16:40:38 +0100

    Základy Javascriptu 3 Objekty

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 // Základy Javascript 3
 
-// Template strings
+// Objekty
 
-let myName = "Harry Potter"
-let age = 15
-console.log("Volam sa " + myName + " a mam " + age + " rokov")
-console.log(`Volam sa ${myName} a mam ${age} rokov`)
-
-let pozdrav = function(firstName, lastName){
-    console.log(`Ahoj, moje meno je ${firstName} a moje priezvisko je ${lastName}`)
+let myBook = {
+    title: "Silmarillion",
+    author: "J. R. R. Tolkien",
+    published: 1977,
+    award: "Locus Award for Best Fantasy Novel"
 }
 
-pozdrav("David", "Golias")
+// Výpis jednotlivých vlastností
+console.log(myBook.title)
+console.log(myBook.author)
+console.log(myBook.published)
+console.log(myBook.award)
+
+// Výpis všetkých vlastností naraz
+console.log(`${myBook.title} je kniha autora ${myBook.author}, bola vydana v roku ${myBook.published} a ziskala cenu ${myBook.award}`)
+
+// Zmena vlastnosti objektu
+myBook.title = "The Hobbit"
+myBook.published = 1937
+
+console.log(`${myBook.title} je kniha od autora ${myBook.author} a bola vydana v roku ${myBook.published}`)
+
+
+
+
+
+
+
 
 
